ce495b592592e4e4bd4795f852a88ea5972ff92e
Harsha <58979824+HarshaMupparaju@users.noreply.github.com>
2026-04-27 13:27:20 -0400

    Add: partition-equal-subset-sum - submission-1

---
+++ Data Structures & Algorithms/partition-equal-subset-sum/submission-1.py
@@ -0,0 +1,40 @@
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        n = len(nums)
+
+        s = sum(nums)
+
+        if(s % 2 == 1):
+            return False # Odd sum can't be partitioned
+        
+        target = s // 2
+
+        memo = {}
+
+        def dfs(i: int, t: int) -> bool:
+            if(t == 0):
+                return True
+            elif(t < 0 ):
+                return False
+            
+            if(i >= n):
+                return False
+
+            if((i, t) in memo):
+                return memo[(i, t)]
+            
+            #Take the current element
+            take = dfs(i + 1, t - nums[i])
+
+            #Don't take the current element
+            skip = dfs(i + 1, t)
+            
+            res = take or skip
+
+            memo[(i, t)] = res
+
+            return res
+
+        res = dfs(0, target)
+
+        return res
