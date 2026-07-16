2d7b2c42688c5a93c170131590886f30598214b7
Harsha <58979824+HarshaMupparaju@users.noreply.github.com>
2026-07-16 15:00:09 -0400

    Add: is-palindrome - submission-3

---
+++ Data Structures & Algorithms/is-palindrome/submission-3.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        
+        #Preprocess the string, strip it of all non=alphanumerical characters and make it lower case
+        
+        temp = []
+
+        for c in s:
+            if( (ord('a') <= ord(c) <= ord('z')) or (ord('A') <= ord(c) <= ord('Z')) or (ord('0') <= ord(c) <= ord('9')) ):
+                temp.append(c.lower())
+        
+        s = ''.join(temp)
+
+        left = 0
+        right = len(s) - 1
+
+        while(left <= right):
+            if(s[left] != s[right]):
+                return False
+            
+            left += 1
+            right -= 1
+
+        return True
