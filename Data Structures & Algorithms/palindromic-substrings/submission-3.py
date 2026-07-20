8799272564342a89a23ef1800dfe339a2fdb23e3
Harsha <58979824+HarshaMupparaju@users.noreply.github.com>
2026-07-20 15:42:53 -0400

    Add: palindromic-substrings - submission-3

---
+++ Data Structures & Algorithms/palindromic-substrings/submission-3.py
@@ -0,0 +1,24 @@
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        res = []
+
+        for i in range(len(s)):
+
+            #Odd Palindromes
+            l = i
+            r = i
+
+            while(l >= 0 and r < len(s) and s[l] == s[r]):
+                res.append(s[l : r + 1])
+                l -= 1
+                r += 1
+
+            #Even Palindromes
+            l = i
+            r = i + 1
+            while(l >= 0 and r < len(s) and s[l] == s[r]):
+                res.append(s[l : r + 1])
+                l -= 1
+                r += 1
+
+        return len(res)
