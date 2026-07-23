e8596bb175b62b758417e33d6ee172d22f261b50
Harsha <58979824+HarshaMupparaju@users.noreply.github.com>
2026-07-23 00:42:21 -0400

    Add: remove-duplicates-from-sorted-array - submission-1

---
+++ Data Structures & Algorithms/remove-duplicates-from-sorted-array/submission-1.py
@@ -0,0 +1,16 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        #TC: O(n); SC: O(1)
+        k = 0
+        ptr = 0
+        #[2, 10, 10, 30, 30, 30]
+        cur = float('-inf')
+        for i in range(len(nums)):
+            if(nums[i] != cur):
+                #New unique element
+                cur = nums[i]
+                nums[ptr] = nums[i]
+                ptr += 1
+                k += 1
+
+        return k
